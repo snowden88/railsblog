02715dbeb5279bb45398d07df9c4d464736bfc5e
Michael <msnowd01@gmail.com>
2016-01-07 19:41:53 -0500

    Posts Controller Update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,15 @@ class PostsController < ApplicationController
 
   def new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
 end
